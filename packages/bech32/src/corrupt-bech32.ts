52ba32faf90847b5b459fa2c10049ed694f4a439
turbocrime <turbocrime@users.noreply.github.com>
2024-04-04 15:35:54 -0700

    last index of 1

--- packages/bech32/src/corrupt-bech32.ts
+++ packages/bech32/src/corrupt-bech32.ts
@@ -1,13 +1,16 @@
 export const corruptBech32 = (goodBech32: string) => {
-  const [hrp, data] = goodBech32.split('1') as [string, string];
+  const separator = goodBech32.lastIndexOf('1');
+  const [hrp, data] = [goodBech32.slice(0, separator), goodBech32.slice(separator + 1)];
 
   const alphabet = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l';
 
   const index = Math.floor(Math.random() * data.length);
   const dontUse = data[index];
-  const wrongChar = Array.from(alphabet).filter(c => c !== dontUse)[
-    Math.floor(Math.random() * (alphabet.length - 1))
-  ];
+  const wrongChar = Array.from(alphabet)
+    .filter(c => c !== dontUse)
+    .sort(Math.random)
+    .pop();
+  const bad = `${data.slice(0, index)}${wrongChar}${data.slice(index + 1)}`;
 
-  return `${hrp}1${data.slice(0, index)}${wrongChar}${data.slice(index + 1)}`;
+  return `${hrp}1${bad}`;
 };
