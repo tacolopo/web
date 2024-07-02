86a91c17fee283a87ffe2d204e7ebba5010c74c6
turbocrime <turbocrime@users.noreply.github.com>
2024-07-02 13:55:16 -0700

    accept destination arg in pack-public.sh

--- pack-public.sh
+++ pack-public.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+packDestination=""
+[ -z "$1" ] || packDestination="--pack-destination $(readlink -f -- "$1")"
+
+cd "$(readlink -f -- "$(dirname "$0")")"
+
 # pnpm doesn't like to recurse for the pack command, and turbo doesn't
-# understand it as a script.  this script should pack every non-private package.
+# understand it as a package script. this script should pack a tarball for every
+# non-private package.
 
 pnpm turbo run build --filter="./packages/*"
 
@@ -9,7 +15,7 @@ for packageJ in packages/*/package.json; do
     package=$(dirname $packageJ)
     private=$(pnpm pkg get private -C "$package")
     if [ "$private" != "true" ]; then
-        pnpm pack -C "$package" &
+        pnpm pack $packDestination -C "$package" &
     fi
 done
 
